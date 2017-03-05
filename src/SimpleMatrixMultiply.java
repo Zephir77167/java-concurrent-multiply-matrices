e4476923010983d869f76c62bb5f78c4407f13d1
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2017-03-05 17:31:36 +0000

    use of timer in main

--- src/SimpleMatrixMultiply.java
+++ src/SimpleMatrixMultiply.java
@@ -9,17 +9,27 @@ public class SimpleMatrixMultiply implements Runnable {
       System.exit(1);
     }
 
+    Timer timer1 = new Timer(), timer2 = new Timer(), timer3 = new Timer();
+
+    timer1.start();
     Matrix[] matrices = MatricesCreator.createMatrices(args[0]);
+    timer1.end();
 
     matrices[0].print();
-    System.out.println();
     matrices[1].print();
-    System.out.println();
 
+    timer2.start();
     Matrix result1 = matrices[0].multiplyBy(matrices[1]);
+    timer2.end();
     result1.print();
-    System.out.println();
+
+    timer3.start();
     Matrix result2 = matrices[1].multiplyBy(matrices[0]);
+    timer3.end();
     result2.print();
+
+    System.out.println("Time spent parsing matrices: " + timer1.getEllapsedTime());
+    System.out.println("Time spent doing m1 * m2: " + timer2.getEllapsedTime());
+    System.out.println("Time spent doing m2 * m1: " + timer3.getEllapsedTime());
   }
 }
