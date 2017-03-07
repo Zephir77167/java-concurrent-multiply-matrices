a4f597b436fb629ce721361a4587d058c0549053
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2017-03-07 00:45:14 +0000

    fixed implementation (back 25s)

--- src/SimpleMatrix.java
+++ src/SimpleMatrix.java
@@ -1,34 +1,28 @@
 class SimpleMatrix extends AMatrix {
   int NB_THREADS = 4;
 
-  AMatrix _m1;
-  AMatrix _m2;
-  long[] _resultArray;
-  int _resultHeight;
-  int _resultWidth;
-  int _diagonal;
+  private AMatrix _m1;
+  private AMatrix _m2;
+  private int _resultHeight;
+  private int _resultWidth;
+  private long[] _resultArray;
 
   SimpleMatrix(int height, int width, long[] array) {
     super(height, width, array);
   }
 
   class LineCalculator implements Runnable {
-    AMatrix _m1;
-    AMatrix _m2;
-    long[] _resultArray;
-    int _resultHeight;
-    int _resultWidth;
-    int _diagonal;
+    int _k;
 
-    LineCalculator(int diagonal) {
-      _diagonal = diagonal;
+    LineCalculator(int k) {
+      _k = k;
     }
 
     public void run () {
       for (int i = 0; i < _resultHeight; ++i) {
         for (int j = 0; j < _resultWidth; ++j) {
           _resultArray[i * _resultWidth + j] +=
-            _m1.getArray()[i * _m1.getWidth() + _diagonal] * _m2.getArray()[_diagonal * _m2.getWidth() + j];
+            _m1.getArray()[i * _m1.getWidth() + _k] * _m2.getArray()[_k * _m2.getWidth() + j];
         }
       }
     }
