47c9b82621765196e71016d5586dd6f58762fe51
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2017-03-07 00:39:22 +0000

    test kij (4s -> 400ms)

--- src/SimpleMatrix.java
+++ src/SimpleMatrix.java
@@ -1,4 +1,13 @@
 class SimpleMatrix extends AMatrix {
+  int NB_THREADS = 4;
+
+  AMatrix _m1;
+  AMatrix _m2;
+  long[] _resultArray;
+  int _resultHeight;
+  int _resultWidth;
+  int _diagonal;
+
   SimpleMatrix(int height, int width, long[] array) {
     super(height, width, array);
   }
@@ -7,48 +16,46 @@ class SimpleMatrix extends AMatrix {
     AMatrix _m1;
     AMatrix _m2;
     long[] _resultArray;
+    int _resultHeight;
     int _resultWidth;
-    int _line;
+    int _diagonal;
 
-    LineCalculator(AMatrix m1, AMatrix m2, long[] resultArray, int resultWidth, int line) {
-      _m1 = m1;
-      _m2 = m2;
-      _resultArray = resultArray;
-      _resultWidth = resultWidth;
-      _line = line;
+    LineCalculator(int diagonal) {
+      _diagonal = diagonal;
     }
 
     public void run () {
-      for (int k = 0; k < _m1.getWidth(); ++k) {
+      for (int i = 0; i < _resultHeight; ++i) {
         for (int j = 0; j < _resultWidth; ++j) {
-          _resultArray[_line * _resultWidth + j] +=
-            _m1.getArray()[_line * _m1.getWidth() + k] * _m2.getArray()[k * _m2.getWidth() + j];
+          _resultArray[i * _resultWidth + j] +=
+            _m1.getArray()[i * _m1.getWidth() + _diagonal] * _m2.getArray()[_diagonal * _m2.getWidth() + j];
         }
       }
     }
   }
 
   AMatrix multiplyBy(AMatrix m2) {
-    AMatrix m1 = this;
+    _m1 = this;
+    _m2 = m2;
+    _resultHeight = _m1.getHeight();
+    _resultWidth = _m2.getWidth();
+    _resultArray = new long[_resultHeight * _resultWidth];
 
-    int resultHeight = m1.getHeight();
-    int resultWidth = m2.getWidth();
-    long[] resultArray = new long[resultHeight * resultWidth];
-    Thread[] threads = new Thread[resultHeight];
+    Thread[] threads = new Thread[_resultHeight];
 
-    for (int i = 0; i < resultHeight; ++i) {
-      threads[i] = new Thread(new LineCalculator(m1, m2, resultArray, resultWidth, i));
-      threads[i].start();
+    for (int k = 0; k < _m1.getWidth(); ++k) {
+      threads[k] = new Thread(new LineCalculator(k));
+      threads[k].start();
     }
 
     try {
-      for (int i = 0; i < resultHeight; ++i) {
+      for (int i = 0; i < _resultHeight; ++i) {
         threads[i].join();
       }
     } catch (InterruptedException e) {
       System.err.println("Thread supposed to compute line has been unexpectedly interrupted");
     }
 
-    return new SimpleMatrix(resultHeight, resultWidth, resultArray);
+    return new SimpleMatrix(_resultHeight, _resultWidth, _resultArray);
   }
 }
