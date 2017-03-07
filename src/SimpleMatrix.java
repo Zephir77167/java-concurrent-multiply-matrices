cd32ff65c72d4d089664d618be8515fdde3697d3
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2017-03-07 01:03:11 +0000

    fixed nb threads

--- src/SimpleMatrix.java
+++ src/SimpleMatrix.java
@@ -11,18 +11,22 @@ class SimpleMatrix extends AMatrix {
     super(height, width, array);
   }
 
-  class LineCalculator implements Runnable {
-    int _k;
+  class SectionCalculator implements Runnable {
+    int _start;
+    int _end;
 
-    LineCalculator(int k) {
-      _k = k;
+    SectionCalculator(int start, int end) {
+      _start = start;
+      _end = end;
     }
 
     public void run () {
-      for (int i = 0; i < _resultHeight; ++i) {
-        for (int j = 0; j < _resultWidth; ++j) {
-          _resultArray[i * _resultWidth + j] +=
-            _m1.getArray()[i * _m1.getWidth() + _k] * _m2.getArray()[_k * _m2.getWidth() + j];
+      for (int k = _start; k < _end; ++k) {
+        for (int i = 0; i < _resultHeight; ++i) {
+          for (int j = 0; j < _resultWidth; ++j) {
+            _resultArray[i * _resultWidth + j] +=
+              _m1.getArray()[i * _m1.getWidth() + k] * _m2.getArray()[k * _m2.getWidth() + j];
+          }
         }
       }
     }
@@ -35,15 +39,15 @@ class SimpleMatrix extends AMatrix {
     _resultWidth = _m2.getWidth();
     _resultArray = new long[_resultHeight * _resultWidth];
 
-    Thread[] threads = new Thread[_m1.getWidth()];
+    Thread[] threads = new Thread[NB_THREADS];
 
-    for (int k = 0; k < _m1.getWidth(); ++k) {
-      threads[k] = new Thread(new LineCalculator(k));
-      threads[k].start();
+    for (int i = 0; i < NB_THREADS; ++i) {
+      threads[i] = new Thread(new SectionCalculator(i * _m1.getWidth() / NB_THREADS, (i + 1) * _m1.getWidth() / NB_THREADS));
+      threads[i].start();
     }
 
     try {
-      for (int i = 0; i < _m1.getWidth(); ++i) {
+      for (int i = 0; i < NB_THREADS; ++i) {
         threads[i].join();
       }
     } catch (InterruptedException e) {
