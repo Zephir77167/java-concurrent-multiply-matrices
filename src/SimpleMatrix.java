c445f52ec1f8037e70ce94322fdeeea49e6ad2e3
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2017-03-07 01:05:56 +0000

    test

--- src/SimpleMatrix.java
+++ src/SimpleMatrix.java
@@ -21,8 +21,8 @@ class SimpleMatrix extends AMatrix {
     }
 
     public void run () {
-      for (int k = _start; k < _end; ++k) {
-        for (int i = 0; i < _resultHeight; ++i) {
+      for (int i = _start; i < _end; ++i) {
+        for (int k = 0; k < _m1.getWidth(); ++k) {
           for (int j = 0; j < _resultWidth; ++j) {
             _resultArray[i * _resultWidth + j] +=
               _m1.getArray()[i * _m1.getWidth() + k] * _m2.getArray()[k * _m2.getWidth() + j];
@@ -40,9 +40,10 @@ class SimpleMatrix extends AMatrix {
     _resultArray = new long[_resultHeight * _resultWidth];
 
     Thread[] threads = new Thread[NB_THREADS];
+    int sectionSize = _resultHeight / NB_THREADS;
 
     for (int i = 0; i < NB_THREADS; ++i) {
-      threads[i] = new Thread(new SectionCalculator(i * _m1.getWidth() / NB_THREADS, (i + 1) * _m1.getWidth() / NB_THREADS));
+      threads[i] = new Thread(new SectionCalculator(i * sectionSize, (i + 1) * sectionSize));
       threads[i].start();
     }
 
